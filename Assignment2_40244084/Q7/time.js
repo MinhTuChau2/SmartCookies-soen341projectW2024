2ec1b5e8a646e0f921a89111b0dd5ab8d7abbea9
Carmoucha <157990722+Carmoucha@users.noreply.github.com>
2024-02-05 13:34:40 -0500

    Delete Assignment2_40244084 directory

--- Assignment2_40244084/Q7/time.js
+++
@@ -1,7 +0,0 @@
-function updateTime() {
-    const now = new Date();
-    const timeString = now.toLocaleString();
-    document.getElementById("timeDisplay").innerText = timeString;
-}
-
-setInterval(updateTime, 1000); 
