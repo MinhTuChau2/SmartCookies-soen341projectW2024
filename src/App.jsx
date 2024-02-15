c78a6daaa8f76889c825f9d317aad86476151f75
unknown <minhtuchau2@gmail.com>
2024-02-14 22:07:31 -0500

    skeleton of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from "./HomePage";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import CarListingPage from './CarListingPage';
+import ContactPage from './ContactPage';
+import Login from './Login';
 import './App.css';
 
-const App = () => {
-
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/CarListingPage">Car Listing</Link></li>
+        <li><Link to="/ContactPage">Contact</Link></li>
+        <li><Link to="/LoginPage">Login</Link></li>
+      </ul>
+    </nav>
+  );
 }
 
+const App = () => {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/CarListingPage" element={<CarListingPage />} />
+        <Route path="/ContactPage" element={<ContactPage />} />
+        <Route path="/LoginPage" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
 export default App;
---
+++ src/CarListingPage.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const CarListingPage = () => {
+    return (
+        <div className='header'>
+            <span className='title'>Carlisting</span>
+        </div>
+    )
+}
+
+export default CarListingPage
