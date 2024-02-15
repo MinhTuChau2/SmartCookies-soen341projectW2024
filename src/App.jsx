6450e6defb638d451d38fdd0842806707a6723f5
MinhTu Chau <minhtuchau2@gmail.com>
2024-02-15 12:58:09 -0500

    problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from "./HomePage";
+import HomePage from './HomePage';
 import './App.css';
 
 const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
