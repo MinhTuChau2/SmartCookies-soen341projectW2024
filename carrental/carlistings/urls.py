a5152dab07d15145dab3baf94cd68d3d1d5dad62
MinhTu Chau <minhtuchau2@gmail.com>
2024-03-15 16:34:34 -0400

    minhtu

--- carrental/carlistings/urls.py
+++ carrental/carlistings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import car_listing
 
 urlpatterns = [
-    path('carlistings', car_listing, name='car_listing'),
+    path('carlistings/', car_listing, name='car_listing'),
 ]
