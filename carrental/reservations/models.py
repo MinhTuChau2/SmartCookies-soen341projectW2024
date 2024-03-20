190eea90469a212e628e61c5bc15064e2e169be8
Hawraa <hawraapixi7@gmail.com>
2024-03-20 02:44:00 -0400

    reservations of the same car at the same time not allowed

--- carrental/reservations/models.py
+++ carrental/reservations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 
 # Create your models here.
 
@@ -8,6 +9,16 @@ class Reservation(models.Model):
     email = models.EmailField()
     pickup_date = models.DateField()
     return_date = models.DateField()
+
+    def clean(self):
+        overlapping_reservations = Reservation.objects.filter(
+            car_model=self.car_model,
+            pickup_date__lte=self.return_date,
+            return_date__gte=self.pickup_date
+        ).exclude(id=self.id)  # Exclude the current instance to allow updates
+
+        if overlapping_reservations.exists():
+            raise ValidationError('This car is already reserved for the given dates.')
 
     class Meta:
         app_label = 'reservations'
--- carrental/reservations/views.py
+++ carrental/reservations/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-# reservations/views.py
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Reservation
 import json
+from django.core.exceptions import ValidationError
 
 @csrf_exempt
 def reserve_car(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        reservation = Reservation.objects.create(
+        
 
-            car_model=data['carModel'],
-            name=data['name'],
-            email=data['email'],
-            pickup_date=data['pickupDate'],
-            return_date=data['returnDate']
-        )
-        return JsonResponse({'message': 'Reservation successful'})
+        try:
+            reservation = Reservation(
+                car_model=data['carModel'],
+                name=data['name'],
+                email=data['email'],
+                pickup_date=data['pickupDate'],
+                return_date=data['returnDate']
+            )
+            reservation.full_clean()  # This will call the clean method and raise ValidationError if any
+            reservation.save()
+            return JsonResponse({'message': 'Reservation successful'})
+
+        except ValidationError as e:
+            return JsonResponse({'error': str(e.message_dict)}, status=400)
+
     else:
-        return JsonResponse({'error': 'Invalid request method'})
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
