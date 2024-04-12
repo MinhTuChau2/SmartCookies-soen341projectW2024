e44003686d880b10df8a342df06ab164dcc8889f
MinhTuChau2 <minhtuchau2@gmail.com>
2024-04-12 14:55:18 -0400

    CRUD USERS

--- carrental/accounts/urls.py
+++ carrental/accounts/urls.py
@@ -3,11 +3,16 @@ from django.urls import path
 from .views import UserRegistrationView, CustomObtainAuthToken  # Ensure these are correctly imported
 from .views import UserDetailView
 from .views import get_user_data
+from .views import UserListView
+
 
 urlpatterns = [
     path('signup/', UserRegistrationView.as_view(), name='signup'),
     path('login/', CustomObtainAuthToken.as_view(), name='login'),
     path('user/', UserDetailView.as_view(), name='user_detail'),
-    path('api/get-user-data/', get_user_data, name='get_user_data')  
+    path('api/users/', UserListView.as_view(), name='user-list'),
+    path('api/get-user-data/', get_user_data, name='get_user_data'),
+    path('api/users/<str:email>/', UserListView.as_view(), name='user-detail')
+   
 ]
 
